c3f77833ca4a99e92ee5d034d51f0dc8d88c7b8b
Divyanshu Mohapatra <divyanshu.m2020bds@srisriuniversity.edu.in>
2022-05-29 20:42:02 +0530

    add mysql2 package and write simple queries to check connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+const mysql = require("mysql2");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -10,6 +11,25 @@ app.get("/", function(req,res){
   res.render("index")
 });
 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password:'password',
+  database: 'ssu'
+});
+
+// simple query
+connection.query(
+  'SELECT results.internal, results.end_sem FROM (Students INNER JOIN results ON Students.id = Results.student_id) INNER JOIN courses ON courses.id = results.course_id WHERE regd_no = "FOS-BDS-2020-23-017"',
+  function(err, results, fields) {
+    if(err){
+      console.log(err);
+    }
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
 
 
 
