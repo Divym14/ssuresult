7b9aef27b71a282e4cc5c5b7f76ff0dc386da907
Divyanshu Mohapatra <divyanshu.m2020bds@srisriuniversity.edu.in>
2022-05-31 19:40:34 +0530

    change the greetings to be dynamic, turn off the autocomplete for the search boxes

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ app.get("/", function(req,res){
   res.render("index")
 });
 
+const greetingsArray = ["Hello","Welcome", "Howdy", "Hola", "Yo!", "Hi!!!", "Hey!!!"];
+
 app.post("/result",function(req,res){
   let rollNumber = req.body.searchTxt;
+  const greetings = getRandomItem(greetingsArray);
   connection.query(
     'SELECT * FROM (Students INNER JOIN results ON Students.id = Results.student_id) INNER JOIN courses ON courses.id = results.course_id WHERE regd_no ="'+rollNumber+'"',
     function(err, results, fields) {
@@ -21,7 +24,7 @@ app.post("/result",function(req,res){
         console.log(err);
       }
       // console.log(results); // results contains rows returned by server
-      res.render("result",{results:results});
+      res.render("result",{results:results, greetings:greetings});
     }
   );
 });
@@ -36,7 +39,17 @@ const connection = mysql.createConnection({
 // simple query
 
 
+// *********************** ALl FUNCTIONS BELOW *************************
+function getRandomItem(arr) {
 
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // get random item
+    const item = arr[randomIndex];
+
+    return item;
+}
 
 
 app.listen(3000, function(){
