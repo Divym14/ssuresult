7f076bcc8f374fb7265237c87837ba3eee1b7f45
Divyanshu Mohapatra <divyanshu.m2020bds@srisriuniversity.edu.in>
2022-06-02 17:41:41 +0530

    add functionallity to show results of semester one time only using drop down

--- app.js
+++ app.js
@@ -12,22 +12,6 @@ app.get("/", function(req,res){
   res.render("index")
 });
 
-const greetingsArray = ["Hello","Welcome", "Howdy", "Hola", "Yo!", "नमस्ते!!", "Hey!!!", "Bonjour"];
-
-app.post("/result",function(req,res){
-  let rollNumber = req.body.searchTxt;
-  const greetings = getRandomItem(greetingsArray);
-  connection.query(
-    'SELECT * FROM (Students INNER JOIN results ON Students.id = Results.student_id) INNER JOIN courses ON courses.id = results.course_id WHERE regd_no ="'+rollNumber+'"',
-    function(err, results, fields) {
-      if(err){
-        console.log(err);
-      }
-      // console.log(results); // results contains rows returned by server
-      res.render("result",{results:results, greetings:greetings});
-    }
-  );
-});
 // create the connection to database
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -36,7 +20,58 @@ const connection = mysql.createConnection({
   database: 'ssu'
 });
 
-// simple query
+const greetingsArray = ["Hello","Welcome", "Howdy", "Hola", "Yo!", "नमस्ते!!", "Hey!!!", "Bonjour"];
+var rollNumber = "";
+var rollArray = [];
+var count = 0;
+
+app.post("/result",function(req,res){
+
+  var semNumber = 1;
+  if (typeof(req.body.searchTxt) === "string") {
+    rollNumber = req.body.searchTxt;
+    rollArray.push(rollNumber);
+    count++;
+  }else{
+    rollNumber = rollArray[count-1];
+  }
+
+  if(Number.isInteger(parseInt(req.body.semester))){
+    semNumber = req.body.semester;
+    
+  }
+  else{
+    semNumber = 1;
+  }
+  console.log(rollNumber);
+  console.log(semNumber);
+
+  const greetings = getRandomItem(greetingsArray);
+  let searchResult = [];
+
+
+  connection.query(
+    'SELECT * FROM (students inner join Dept on students.Dept_id = Dept.id \
+      inner join  results on students.id = results.student_id)\
+     inner join courses on results.course_id = courses.id \
+    WHERE regd_no ="'+ rollNumber+'"'+"AND Results.sem_number="+semNumber,
+    function(err, results, fields) {
+      if(err){
+        console.log(err);
+      }
+      else{
+        searchResult = results;
+      }
+     // console.log(searchResult[0].first_name); // results contains rows returned by server
+      res.render("result",{
+        results:searchResult,
+        greetings:greetings});
+    });
+
+
+});
+
+
 
 
 // *********************** ALl FUNCTIONS BELOW *************************
